0d0fc9603355be7e8eaad383421d7725cb29b6b9
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2024-03-26 17:32:45 +0700

    commit

--- app/pending/_component/Link-Button.tsx
+++ app/pending/_component/Link-Button.tsx
@@ -2,31 +2,36 @@
 
 import Clerk from "@clerk/clerk-js";
 import { UserButton } from "@clerk/nextjs";
+import { Link } from "lucide-react";
+import { useRouter } from "next/navigation";
 const LinkButton = async () => {
-  // const onLink = async () => {
-  //   try {
-  //     const userClerk = new Clerk(
-  //       "pk_test_cG93ZXJmdWwtZm94LTEuY2xlcmsuYWNjb3VudHMuZGV2JA"
-  //     );
-  //     await userClerk.load();
-  //     let user = await userClerk?.user?.createExternalAccount({
-  //       strategy: "oauth_microsoft",
-  //       redirect_url: "/pending",
-  //       redirectUrl: "/pending",
-  //     });
-  //     console.log(user);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const router = useRouter();
+  const onLink = async () => {
+    try {
+      const userClerk = new Clerk(
+        "pk_test_cG93ZXJmdWwtZm94LTEuY2xlcmsuYWNjb3VudHMuZGV2JA"
+      );
+      await userClerk.load();
+      let user: any = await userClerk?.user?.createExternalAccount({
+        strategy: "oauth_microsoft",
+        redirect_url: "/pending",
+        redirectUrl: "/pending",
+      });
+      console.log(user.verification.externalVerificationRedirectURL.href);
+      router.push(user.verification.externalVerificationRedirectURL.href);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       Please link your account to your Lien Phat microsoft account here
-      <UserButton
+      <button onClick={() => onLink()}>Link</button>
+      {/* <UserButton
         afterSignOutUrl="/"
         defaultOpen={true}
         userProfileMode="modal"
-      />
+      /> */}
     </>
   );
 };
