bb4579acdffd444352d47021cbb91b0f8e24ff06
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2024-03-28 16:04:15 +0700

    commit

--- app/(dashboard)/(routes)/teacher/users/page.tsx
+++ app/(dashboard)/(routes)/teacher/users/page.tsx
@@ -8,12 +8,19 @@ import { columns } from "./_components/columns";
 import { getUser } from "@/actions/get-user";
 
 const UsersPage = async () => {
-  const { userId } = auth();
-
+  const { userId, sessionClaims }: any = auth();
+  let userInfo: any = await db.user.findUnique({
+    where: { id: userId },
+  });
   if (!userId) {
     return redirect("/");
   }
-
+  if (sessionClaims.userInfo.staff) {
+    return redirect("/");
+  }
+  if (userInfo.status == "pending") {
+    return redirect("/pending");
+  }
   const users: any = await getUser();
   return (
     <div className="p-6">
