3ea089bf2c9ff17359bda8ac37e68f1cf793050b
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2024-04-16 17:36:28 +0700

    commit

--- app/(dashboard)/(routes)/teacher/users/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/users/_components/columns.tsx
@@ -1,26 +1,11 @@
+// @refresh reset
 "use client";
 import { User } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import {
-  ArrowUpDown,
-  MoreHorizontal,
-  Pencil,
-  BadgeCheck,
-  Ban,
-  BadgeX,
-} from "lucide-react";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import axios from "axios";
-import Image from "next/image";
+import { ArrowUpDown } from "lucide-react";
 
-import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { Cell } from "@/components/ui/cell";
 
 export const columns: ColumnDef<User>[] = [
   {
@@ -120,75 +105,6 @@ export const columns: ColumnDef<User>[] = [
   {
     id: "actions",
     accessorKey: "Action",
-    cell: ({ row }) => {
-      const { id, status, role } = row.original;
-      const router = useRouter();
-      async function onChangeStatus(id: string, status: string): Promise<void> {
-        let values = {
-          status: status == "approved" ? "pending" : "approved",
-        };
-
-        await axios.patch(`/api/user/${id}/status`, values);
-        router.refresh();
-      }
-      async function onDelete(id: string): Promise<void> {
-        await axios.delete(`/api/user/${id}`);
-        router.refresh();
-      }
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-4 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <Link href={`/teacher/users/${id}`}>
-              <DropdownMenuItem>
-                <Pencil className="h-4 w-4 mr-2" />
-                Edit
-              </DropdownMenuItem>
-            </Link>
-            {role != "ADMIN" ? (
-              status == "approved" ? (
-                <DropdownMenuItem>
-                  <div
-                    onClick={() => onChangeStatus(id, status)}
-                    className="flex items-center"
-                  >
-                    <Ban className="h-4 w-4 mr-2" />
-                    <span>Ban</span>
-                  </div>
-                </DropdownMenuItem>
-              ) : (
-                <div>
-                  <DropdownMenuItem>
-                    <div
-                      onClick={() => onChangeStatus(id, status)}
-                      className="flex items-center"
-                    >
-                      <BadgeCheck className="h-4 w-4 mr-2" />
-                      Approved
-                    </div>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <div
-                      onClick={() => onDelete(id)}
-                      className="flex items-center"
-                    >
-                      <BadgeX className="h-4 w-4 mr-2" />
-                      Delete
-                    </div>
-                  </DropdownMenuItem>
-                </div>
-              )
-            ) : (
-              <></>
-            )}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
+    cell: Cell,
   },
 ];
---
+++ components/ui/cell.tsx
@@ -0,0 +1,88 @@
+"use client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import axios from "axios";
+import {
+  ArrowUpDown,
+  MoreHorizontal,
+  Pencil,
+  BadgeCheck,
+  Ban,
+  BadgeX,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { useAuth } from "@clerk/nextjs";
+export const Cell = ({ row }: any) => {
+  const { id, status, role } = row.original;
+  const router = useRouter();
+  const { userId }: any = useAuth();
+  async function onChangeStatus(id: string, status: string): Promise<void> {
+    let values = {
+      status: status == "approved" ? "pending" : "approved",
+    };
+
+    await axios.patch(`/api/user/${id}/status`, values);
+    router.refresh();
+  }
+  async function onDelete(id: string): Promise<void> {
+    await axios.delete(`/api/user/${id}`);
+    router.refresh();
+  }
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-4 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <Link href={`/teacher/users/${id}`}>
+          <DropdownMenuItem>
+            <Pencil className="h-4 w-4 mr-2" />
+            Edit
+          </DropdownMenuItem>
+        </Link>
+        {userId != id && role != "ADMIN" ? (
+          status == "approved" ? (
+            <DropdownMenuItem>
+              <div
+                onClick={() => onChangeStatus(id, status)}
+                className="flex items-center"
+              >
+                <Ban className="h-4 w-4 mr-2" />
+                <span>Ban</span>
+              </div>
+            </DropdownMenuItem>
+          ) : (
+            <div>
+              <DropdownMenuItem>
+                <div
+                  onClick={() => onChangeStatus(id, status)}
+                  className="flex items-center"
+                >
+                  <BadgeCheck className="h-4 w-4 mr-2" />
+                  Approved
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <div onClick={() => onDelete(id)} className="flex items-center">
+                  <BadgeX className="h-4 w-4 mr-2" />
+                  Delete
+                </div>
+              </DropdownMenuItem>
+            </div>
+          )
+        ) : (
+          <></>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
