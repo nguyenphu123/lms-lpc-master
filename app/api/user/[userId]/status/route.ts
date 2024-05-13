f8f369134eb57fc932409688e7afd7057976231a
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2024-05-13 17:51:01 +0700

    commit

--- app/api/user/[userId]/status/route.ts
+++ app/api/user/[userId]/status/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { clerkClient } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
+const nodemailer = require("nodemailer");
+const smtpTransport = require("nodemailer-smtp-transport");
 export async function PATCH(
   req: Request,
   { params }: { params: { userId: string } }
@@ -20,7 +22,42 @@ export async function PATCH(
         status: values.status,
       },
     });
+    const mess = {
+      from: "Webmaster@lp.com.vn",
+      to: user.email,
+      cc: "",
+      subject: `you have been ${values.status} the system`,
+      text: `
+      you have been ${values.status} the system
+      `,
+      html: `
+        <p>you have been ${values.status} the system.</p>
+        
+      `,
+    };
+    let transporter = nodemailer.createTransport(
+      smtpTransport({
+        host: "smtp-mail.outlook.com",
+        secureConnection: false, // TLS requires secureConnection to be false
+        port: 587, // port for secure SMTP
+        auth: {
+          user: "Webmaster@lp.com.vn",
+          pass: "Lpc@236238$",
+        },
+        tls: {
+          ciphers: "SSLv3",
+        },
+      })
+    );
 
+    try {
+      //send email
+      const res = await transporter.sendMail(mess);
+
+      // return res.status(200).json({ success: true });
+    } catch (err) {
+      console.log("Mail send: ", err);
+    }
     return NextResponse.json(user);
   } catch (error) {
     console.log("[PROGRAMS]", error);
